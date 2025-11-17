b1d449d61c56f110778c237b2cd62adf8c910141
Luiz <luiz.souza@gmail.com>
2025-11-17 18:41:41 -0300

    test: Ajusta ProdutoControllerTest e remove métodos deprecated

---
+++ src/test/java/br/gov/caixa/painelinvestimentos/PainelInvestimentosApplicationTest.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.painelinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelInvestimentosApplicationTest {
+
+    @Test
+    void contextoDeveCarregarSemErros() {
+        // se o contexto subir, o teste passa
+    }
+}
